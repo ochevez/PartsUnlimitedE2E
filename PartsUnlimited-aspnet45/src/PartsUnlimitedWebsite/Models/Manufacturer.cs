ff4ff2a39606a19f17a48dd19aa60f9b9c2e5354
Oscar Chevez <oscar.chevez@grupobusiness.it>
2022-07-27 17:53:58 -0600

    Actualizando clase Manufacturer

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/Manufacturer.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/Manufacturer.cs
@@ -4,6 +4,7 @@ namespace PartsUnlimited.Models
 {
     public class Manufacturer
     {
+        // Modificando cs denominado Manufacturer
         public int ManufacturerId { get; set; }
 
         [Required]
